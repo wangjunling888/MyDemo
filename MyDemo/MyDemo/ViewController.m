103ee0004f8e3ee7fde46ae2763864f8ac6f8360
王俊岭 <987424476@qq.com>
2017-09-30 14:34:28 +0800

    创建项目并初始化

---
+++ MyDemo/MyDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDemo
+//
+//  Created by 王俊岭 on 2017/9/30.
+//  Copyright © 2017年 王俊岭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MyDemo/MyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyDemo
+//
+//  Created by 王俊岭 on 2017/9/30.
+//  Copyright © 2017年 王俊岭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
